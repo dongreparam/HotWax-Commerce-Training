8fd0aa79cdb49ff6e9570d4996af9a658bfbdc23
dongreparam <param.dongre@gmail.com>
2024-12-26 11:33:14 +0530

    Made changes to OOP2.java

---
+++ Learning-Java/Java-Core/Objects-Classes-Methods/OOP2.java
@@ -0,0 +1,12 @@
+class Animal2{
+    String color;
+    int age;
+
+    public static void main(String[] args) {
+        Animal2 buzo = new Animal2();
+        buzo.age = 7;
+        buzo.color = "Black";
+
+        System.out.println(buzo.age + " " + buzo.color);
+    }
+}
