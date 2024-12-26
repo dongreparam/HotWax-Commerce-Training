bb15858230690660bfd6be3f8b6c3450bd8361a9
dongreparam <param.dongre@gmail.com>
2024-12-26 10:35:37 +0530

    Deploy code to new branch

---
+++ OOP1.java
@@ -0,0 +1,12 @@
+class Oops1 {
+
+    void display(){
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+
+        Oops1 obj = new Oops1();
+        obj.display();
+    }
+}
