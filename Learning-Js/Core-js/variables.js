43a06eef6f1e4b4beaf88d109f1b6fb87fd27b8c
dongreparam <param.dongre@gmail.com>
2025-06-14 15:46:25 +0530

    Learning Variables

--- Learning-Js/Core-js/variables.js
+++ Learning-Js/Core-js/variables.js
@@ -1,3 +1,26 @@
 let name = 'Param'
 console.log(name)
 
+let isStudent = true
+let score = 90
+let hobbies = ['coding', 'reading', 'writing']
+
+console.log(hobbies)
+
+let emptyBox = null
+
+console.log(typeof(emptyBox))
+
+let person = {
+    name: 'Param',
+    age: 20,
+    isStudent: true
+}
+
+console.log(person)
+
+console.log(Object.keys(person))
+
+console.log(Object.values(person))
+
+console.log(Object.entries(person)[0])
